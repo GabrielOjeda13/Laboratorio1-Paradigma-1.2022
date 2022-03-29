9e3b7b92d992e9cfce6785e62cb486b12a064814
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-03-28 23:31:42 -0300

    Segundo Commit
    Creacion de Funciones para concatenar una lista y para mapear una lista

--- main.rkt
+++ main.rkt
@@ -1,2 +1,27 @@
 #lang racket
 (provide (all-defined-out))
+
+
+;Funcion Concatenar: Concatena dos listas
+;Dom: Lista x lista
+;Rec: Lista
+;Recursion: Natural
+(define (concatenar lista1 lista2)
+  (if(null? lista1)
+     lista2
+     (cons (car lista1) (concatenar (cdr lista1) lista2))
+  )
+)
+
+;Funcion mi-map: sirve para hacer lo mismo que hace la funcion MAP 
+;Dom: Lista
+;Rec: Lista
+;Recursion: cola
+;Ejemplo de uso:
+(define mi-map (lambda(funcion lista)
+                 (if(empty? lista) '()
+
+                    (cons (funcion (car lista)) (mi-map funcion (cdr lista)))
+                    )
+                 )
+  )
